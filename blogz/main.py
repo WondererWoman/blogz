47a920d83c579a90fdcf5f6e8b2757fd6db2b014
Erin DeVries <erindevrieswebdev@gmail.com>
2017-02-27 17:11:35 -0600

    Fixed getpostsbyuser

--- blogz/main.py
+++ blogz/main.py
@@ -14,15 +14,15 @@ class BlogHandler(webapp2.RequestHandler):
         query = Post.all().order('-created')
         return query.fetch(limit=limit, offset=offset)
 
-    def get_posts_by_user(self, limit, offset):
+    def get_posts_by_user(self, user, limit, offset):
         """
             Get all posts by a specific user, ordered by creation date (descending).
             The user parameter will be a User object.
         """
 
         # TODO - filter the query so that only posts by the given user
-        query = Post.all().filter('author=', 'author').order('-created')
-        return query.fetch(username=username, limit=limit, offset=offset)
+        query = Post.all().filter('author', user).order('-created')
+        return query.fetch(limit=limit, offset=offset)
 
     def get_user_by_name(self, username):
         """ Get a user object from the db, based on their username """
